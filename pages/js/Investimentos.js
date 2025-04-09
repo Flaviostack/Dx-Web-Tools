175986b6c543964ec93257631fdc6f9108c31dfb
Flaviostack <fa3196318@gmail.com>
2025-04-09 11:10:35 -0300

    Adicionada a opção de aportes mensais e troca da Ui

--- pages/js/Investimentos.js
+++ pages/js/Investimentos.js
@@ -2,20 +2,40 @@ function calcularInvestimento() {
     var valorInicial = parseFloat(document.getElementById("ValorInicial").value);
     var taxa = parseFloat(document.getElementById("Taxa").value);
     var tempo = parseInt(document.getElementById("Tempo").value);
+    var aporteMensal = parseFloat(document.getElementById("AporteMensal").value);
 
-    if (isNaN(valorInicial) || isNaN(taxa) || isNaN(tempo)) {
+    if (isNaN(valorInicial) || isNaN(taxa) || isNaN(tempo) || valorInicial <= 0 || taxa <= 0 || tempo <= 0) {
         alert("Por favor, preencha todos os campos corretamente.");
         return;
     }
 
-    var montante = CalcularCDB(valorInicial, taxa, tempo);
-    document.getElementById("Valor-final").innerText = "R$ " + montante.toFixed(2);
+    // Chama a função CalcularCDB e passa todos os valores necessários
+    var resultados = CalcularCDB(valorInicial, taxa, tempo, aporteMensal);
+
+    // Atualiza os elementos no HTML com os resultados
+    document.getElementById("Juros-Simples").innerText = "Juros Simples: R$ " + resultados.jurosSimples.toFixed(2);
+    document.getElementById("Juros-Compostos").innerText = "Juros Compostos: R$ " + resultados.jurosCompostos.toFixed(2);
 }
 
-function CalcularCDB(valorInicial, taxa, tempo) {
+function CalcularCDB(valorInicial, taxa, tempo, aporteMensal) {
     // Cálculo do rendimento de um CDB
-    // Fórmula: Montante = Valor Aplicado * (1 + Taxa de Juros) ^ Prazo
-    var montante = valorInicial * Math.pow((1 + (taxa / 100)), tempo);
-    //alert("O montante após " + tempo + " meses é: R$" + montante.toFixed(2));
-    return montante;
+    if (aporteMensal > 0) {
+        // Se houver aporte mensal, calcula o montante total
+        var montanteTotal = valorInicial * Math.pow((1 + (taxa / 100)), tempo) + (aporteMensal * ((Math.pow((1 + (taxa / 100)), tempo) - 1) / (taxa / 100)));
+        return {
+            jurosSimples: montanteTotal,
+            jurosCompostos: montanteTotal
+        };
+    }
+
+    if (aporteMensal == 0 || isNaN(aporteMensal)) {
+        var montanteJurosSimples = valorInicial * (1 + (taxa / 100) * tempo);
+        var montanteJurosCompostos = valorInicial * Math.pow((1 + (taxa / 100)), tempo);
+    }
+
+    // Retorna ambos os valores como um objeto
+    return {
+        jurosSimples: montanteJurosSimples,
+        jurosCompostos: montanteJurosCompostos
+    };
 }
