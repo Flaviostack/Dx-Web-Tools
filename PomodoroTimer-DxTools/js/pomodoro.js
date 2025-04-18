3fff53c29da06a50c4b409f7a0e2ec6f90003b87
Flaviostack <fa3196318@gmail.com>
2025-04-18 20:17:27 -0300

    adicionado SEO básico

--- PomodoroTimer-DxTools/js/pomodoro.js
+++ PomodoroTimer-DxTools/js/pomodoro.js
@@ -1,11 +1,14 @@
-let favicon = document.getElementById("favicon"); // Seleciona o favicon
 let GetTime = document.getElementById("timerDisplay"); // Elemento para exibir o tempo
 let timerInterval; // Variável para armazenar o intervalo do timer
-let icon = document.getElementById("icone"); // Elemento do ícone
+
+let startAudio = new Audio("./assets/sound/start.mp3"); startAudio.volume = 0.5;
+
 
 function startTimer(duration) {
+    
     let startTime = Date.now();
     let endTime = startTime + duration * 1000;
+    startAudio.play(); // Toca o áudio de início
 
     function updateTimer() {
         let now = Date.now();
@@ -14,25 +17,9 @@ function startTimer(duration) {
         if (remainingTime <= 0) {
             clearInterval(timerInterval);
             GetTime.innerHTML = "00:00";
-            if (favicon) {
-                favicon.href = "./assets/img/stoped.png"; // Altera o favicon para o ícone de "parado"
-            }
-            if (icon) {
-                icon.src = "./assets/img/stoped.png"; // Caminho corrigido para o ícone parado
-            } else {
-                console.error("Elemento com ID 'icone' não encontrado.");
-            }
             document.title = "Acabou!";
             alert("O tempo acabou!");
         } else {
-            if (favicon) {
-                favicon.href = "./assets/img/clock.svg"; // Altera o favicon para o ícone de "ativo"
-            }
-            if (icon) {
-                icon.src = "./assets/img/clock.svg"; // Caminho corrigido para o ícone ativo
-            } else {
-                console.error("Elemento com ID 'icone' não encontrado.");
-            }
             let minutes = Math.floor(remainingTime / 60);
             let seconds = remainingTime % 60;
             GetTime.innerHTML = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
@@ -54,7 +41,4 @@ function stopTimer() {
 function resetTimer() {
     clearInterval(timerInterval); // Para o timer
     GetTime.innerHTML = "25:00"; // Reseta o display para 25 minutos
-    if (icon) {
-        icon.src = "./assets/img/clock.svg"; // Reseta o ícone para o padrão
-    }
 }
